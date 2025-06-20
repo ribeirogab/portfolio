7d2de9d31a5021cdf2f421b9f9690b20891ea634
ribeirogab <gblosr@gmail.com>
2025-06-20 17:06:34 -0300

    feat: change default locale to english in i18n configuration

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export const locales = ['en', 'pt'] as const;
-export const defaultLocale = 'pt' as const;
+export const defaultLocale = 'en' as const;
 
 export type Locale = (typeof locales)[number];
